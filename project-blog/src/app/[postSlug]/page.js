a9045826cfd16d4741ff863e0df4a2cc765554bc
cadolphe <christopher.adolphe@gmail.com>
2024-03-16 23:59:53 +0400

    feat: improve MDX components map imports

--- project-blog/src/app/[postSlug]/page.js
+++ project-blog/src/app/[postSlug]/page.js
@@ -4,9 +4,7 @@ import { MDXRemote } from 'next-mdx-remote/rsc';
 import { loadBlogPost } from '@/helpers/file-helpers';
 
 import BlogHero from '@/components/BlogHero';
-import CodeSnippet from '@/components/CodeSnippet';
-import DivisionGroupsDemo  from '@/components/DivisionGroupsDemo';
-import CircularColorsDemo from '@/components/CircularColorsDemo';
+import COMPONENT_MAP from '@/helpers/mdx-component';
 
 import styles from './postSlug.module.css';
 
@@ -31,11 +29,7 @@ async function BlogPost({ params }) {
       <div className={styles.page}>
         <MDXRemote
           source={content}
-          components={ {
-            pre: CodeSnippet,
-            DivisionGroupsDemo,
-            CircularColorsDemo,
-          } }
+          components={ COMPONENT_MAP }
         />
       </div>
     </article>
